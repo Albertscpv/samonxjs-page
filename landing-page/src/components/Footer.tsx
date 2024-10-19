f0dc430d0db8d1c53046d16ca9d9c86074072e83
chrisMMsupplier2020 <christophermongemarin@gmail.com>
2024-10-19 22:27:47 +0000

    About us wireframe changes

--- landing-page/src/components/Footer.tsx
+++ landing-page/src/components/Footer.tsx
@@ -2,13 +2,13 @@ import { InstagramIcon, FacebookIcon, TwitterIcon} from './IconsManager'
 const Footer = () => {
     return(
         <>
-            <div className='bg-[#0A0A0A]/30 shadow-md p-4 pb- backdrop-blur-sm flex justify-between'>
-                <span>Powered By Samo D</span>
+            <div className='bg-[#0A0A0A]/30 shadow-md p-4 mt-10 backdrop-blur-sm flex justify-between'>
+                <span className='font-bold'>Powered By Samo D</span>
             <div className='flex text-black justify-end'>
                 <ul className='flex flex-row gap-6'>
-                    <li><a href=""><InstagramIcon/></a></li>
-                    <li><a href=""><FacebookIcon/></a></li>
-                    <li><a href=""><TwitterIcon/></a></li>
+                    <li><a href="/"><InstagramIcon/></a></li>
+                    <li><a href="/"><FacebookIcon/></a></li>
+                    <li><a href="/"><TwitterIcon/></a></li>
                 </ul>
             </div>
             </div>

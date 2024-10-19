a8ebdd4175cf5b61b37e8e9e1962863530755178
chrisMMsupplier2020 <christophermongemarin@gmail.com>
2024-10-19 21:53:38 +0000

    Added animations and Changed TWCSS File to support

--- landing-page/src/app/page.tsx
+++ landing-page/src/app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
     <div className="min-h-screen">
         <div id="main-container" className="relative h-screen p-2">
           <div className="[grid-area:main] p-12 place-content-center text-center">
-              <h2 className="font-bold text-6xl font-geistS text-white">Bienvenidos a Samo D</h2>
-              <span className="block text-xl">Innovación en soluciones, eficiencia en resultados</span>
+              <h2 className="font-bold text-6xl font-geistS text-white  animate-fade-in">Bienvenidos a Samo D</h2>
+              <span className="block text-xl my-2">Innovación en soluciones, eficiencia en resultados.</span>
               <Link href="/about">
-                <button className="p-4 bg-white text-black my-6 hover:bg-black hover:text-white transition duration-600 hover:border">
+                <button className="p-4 bg-[#ededed] text-black my-6 border  hover:bg-black hover:text-white transition duration-600 hover:border animate-fade-in-right">
                   Get Started
                 </button>
               </Link>
